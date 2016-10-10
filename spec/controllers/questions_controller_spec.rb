f863507cf676898f9f92af994ac37b49782fdcfa
Alexandr <microposts@gmail.com>
2016-10-10 22:04:49 +0300

    spec questions_controller#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,14 +4,28 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
+    before { get :index }
+
     it 'populates an array of all questions' do
-      get :index
       expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'render view index' do
-      get :index
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'render view show' do
+      expect(response).to render_template :show
     end
   end
 end
