e770c0b60c8175db7b0a41183c289be3d56fe5b0
Alexandr <microposts@gmail.com>
2016-10-12 22:55:17 +0300

    The user can create an answer to the question

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'POST #create' do
+    let(:question) { create(:question) }
 
+    context 'with valid attributes' do
+      it 'save the new answer in the database' do
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirect to view show' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+
+    context 'with invalid attributes' do
+
+    end
+  end
 end
