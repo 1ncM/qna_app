cd33f37691e5bf9de38369584830a477bcbaa0f2
Alexandr <microposts@gmail.com>
2016-10-13 22:37:56 +0300

    refact sign in

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -6,12 +6,9 @@ feature 'User sign in', %q{
   I want to be able to sign in
 } do
   given(:user) { create(:user) }
-  
+
   scenario 'Registered user try to sign in' do
-    visit new_user_session_path
-    fill_in 'Email', with: user.email 
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+    sign_in(user)
     expect(page).to have_content 'Signed in successfully.'
     expect(current_path).to eq root_path
   end
