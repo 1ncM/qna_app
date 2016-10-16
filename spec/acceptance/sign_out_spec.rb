7443c181b7ee963adb4cd172e88f6714f8ae425e
Alexandr <microposts@gmail.com>
2016-10-16 15:31:42 +0300

    user can sign out

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to use the site on public computers
+  As signed-in User
+  I want to sign out
+} do
+  given(:user) { create(:user) }
+
+  scenario 'authenticated user can sign out' do
+    sign_in(user)
+    visit questions_path
+    expect(page).to have_selector(:link_or_button, 'Sign out')
+    expect(page).to_not have_selector(:link_or_button, 'Sign in')
+    expect(page).to_not have_selector(:link_or_button, 'Sign up')
+    click_on 'Sign out'
+    expect(page).to have_content('Signed out successfully.')
+    expect(page).to have_selector(:link_or_button, 'Sign in')
+    expect(page).to have_selector(:link_or_button, 'Sign up')
+    expect(page).to_not have_selector(:link_or_button, 'Sign out')
+  end
+end
